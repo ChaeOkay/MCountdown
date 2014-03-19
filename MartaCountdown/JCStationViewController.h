8a13cdceb1c9ac35c6996f65bf9dbeff0ae39a47
cokeefe <chaemobile@gmail.com>
2014-03-19 14:19:49 -0400

    Rename station xib and create view controller. Magic

---
+++ MartaCountdown/JCStationViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCStationViewController.h
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/19/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCStationViewController : UIViewController
+
+@end
---
+++ MartaCountdown/JCStationViewController.m
@@ -0,0 +1,13 @@
+//
+//  JCStationViewController.m
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/19/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import "JCStationViewController.h"
+
+@implementation JCStationViewController
+
+@end
