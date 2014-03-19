b0f1812061748102fd97595189ca342b27687d8e
cokeefe <chaemobile@gmail.com>
2014-03-18 20:56:18 -0400

    Initial Commit

---
+++ MartaCountdown/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/18/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MartaCountdown/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/18/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+    }
+}
