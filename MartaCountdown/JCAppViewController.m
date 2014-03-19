2297e70af294477bda389dc5074735a9e69f4af6
cokeefe <chaemobile@gmail.com>
2014-03-19 14:21:12 -0400

    Add two stations to app scroll view, paginate

--- MartaCountdown/JCAppViewController.m
+++ MartaCountdown/JCAppViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "JCAppViewController.h"
+#import "JCStationViewController.h"
 
 @interface JCAppViewController ()
 
@@ -15,5 +16,27 @@
 @end
 
 @implementation JCAppViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    _stationsViewer.pagingEnabled = YES;
+
+    CGRect scrollFrame = self.view.bounds;
+    scrollFrame.size.width *= 3.0;
+
+    for (int i = 0; i < 3; i++){
+        JCStationViewController *stationController = [JCStationViewController new];
+
+        UIView *stationView = stationController.view;
+
+        int x = stationView.frame.size.width * i;
+
+        stationView.frame = CGRectMake(x, stationView.frame.origin.y, stationView.frame.size.width, stationView.frame.size.height);
+
+        [_stationsViewer addSubview:stationController.view];
+    }
+    _stationsViewer.contentSize = scrollFrame.size;
+}
 
 @end
