c4a6765266d4e4cd7a89ac9f622e8b4ada8f76d5
cokeefe <chaemobile@gmail.com>
2014-03-22 12:47:29 -0400

    Create file parsing model

---
+++ MartaCountdown/JCParse.h
@@ -0,0 +1,13 @@
+//
+//  JCParse.h
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/22/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCParse : NSObject
+
+@end
---
+++ MartaCountdown/JCParse.m
@@ -0,0 +1,13 @@
+//
+//  JCParse.m
+//  MartaCountdown
+//
+//  Created by Chae O'Keefe on 3/22/14.
+//  Copyright (c) 2014 ChaeOkay. All rights reserved.
+//
+
+#import "JCParse.h"
+
+@implementation JCParse
+
+@end
